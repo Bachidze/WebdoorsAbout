db40b19fa27a4245a6d34b682955d161fcbdf862
bachidze <bachidze90@gmail.com>
2024-02-29 00:04:16 +0400

    Feat(Remove):Break-All

--- src/Components/LittleComponents/MainParagraph.jsx
+++ src/Components/LittleComponents/MainParagraph.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MainParagraph = ({maindesc}) => {
   return (
     <>
-    <section className='pt-[37.65px] break-all word'>
+    <section className='pt-[37.65px]  word'>
             <p className='text-[16px] text-white leading-[20px] md:text-[18px] md:leading-[22.5px] md:max-w-[1200px] md:m-auto xl:text-[20px] xl:leading-[25px] xl:text-center xl:max-w-[1021px] xl:mb-[90px]'>
                 {maindesc}
             </p>
